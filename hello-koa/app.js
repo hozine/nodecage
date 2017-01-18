98786fc4ac057ce4ce75bb851dde51b574a5f1bc
hozine office <hottyzn@hotmail.com>
2017-01-18 15:22:07 +0800

    add 3 middleware proccess chains

--- hello-koa/app.js
+++ hello-koa/app.js
@@ -1,10 +1,30 @@
 const Koa=require('koa');
 const app=new Koa();
 
+//增加3个middleware组成处理链，依次打印日志，记录处理时间，输出HTML
 app.use(async (ctx,next)=>{
+    console.log(`${ctx.request.method} ${ctx.request.url}`);
+    await next();
+    console.log('app OVER!!!')
+});
+
+app.use(async (xtx,next)=>{
+    const start=new Date().getTime();
+    await next();
+    const ms=new Date().getTime()-start;
+    console.log(`Time:${ms}ms`);
+});
+
+app.use(async (ctx,next)=>{
+    console.log('response start!')
     await next();
     ctx.response.type='text/html';
     ctx.response.body='<h1>Hello, koa2!</h1>';
+    console.log('response over');
+});
+
+app.use(async (ctx,next)=>{
+    console.log("\tI'm in there");
 });
 
 app.listen(3000);
